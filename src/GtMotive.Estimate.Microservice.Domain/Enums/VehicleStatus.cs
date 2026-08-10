ee8c9004fbd024157106eb8511f5b40bb4327811
Pablo del Campo <71131382+PablodlCampo@users.noreply.github.com>
2026-08-10 21:13:06 +0200

    create vehicle entity (#3)

---
+++ src/GtMotive.Estimate.Microservice.Domain/Enums/VehicleStatus.cs
@@ -0,0 +1,23 @@
+﻿namespace GtMotive.Estimate.Microservice.Domain.Enums
+{
+    /// <summary>
+    /// Represents the status of a vehicle, indicating whether it is available for rent or currently rented.
+    /// </summary>
+    public enum VehicleStatus
+    {
+        /// <summary>
+        /// Indicates that the vehicle has no specific status (default value).
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Indicates that the vehicle is available for rent.
+        /// </summary>
+        Available = 1,
+
+        /// <summary>
+        /// Indicates that the vehicle is currently rented and not available for rent.
+        /// </summary>
+        Rented = 2
+    }
+}
